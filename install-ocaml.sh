13c11766b0822c98361b8e6d73aa4e4fa111ee08
leehahoon <leehahoon1001@gmail.com>
2025-08-26 02:41:43 +0000

    update install-ocaml.sh & Dockerfile

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OCAML_VERSION=5.1.0
+export OCAML_VERSION=5.2.0
 export OPAM_SWITCH=prosyslab-classroom-$OCAML_VERSION
 export OPAMYES=1
 
@@ -18,7 +18,7 @@ if ! opam switch list -s | grep -q "^$OPAM_SWITCH\$"; then
 fi
 
 eval $(SHELL=bash opam env --switch=$OPAM_SWITCH)
-opam install -y utop dune llvm ounit merlin ocamlformat=0.26.0 ocaml-lsp-server odoc z3 ocamlgraph core bisect_ppx
+opam install -y utop dune llvm ounit merlin ocamlformat ocaml-lsp-server odoc z3 ocamlgraph core bisect_ppx ocurl
 opam pin add git+https://github.com/prosyslab-classroom/llvmutils.git
 opam pin add prosys-cil https://github.com/prosyslab/cil.git
 opam pin add git+https://github.com/prosyslab-classroom/checkml.git
