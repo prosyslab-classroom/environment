4fff4b255c0afe3221a2de970147b05b0a1a9a1e
Bongjun <bongjun.jang@kaist.ac.kr>
2026-09-07 17:25:54 +0900

    [Pyinfra] checkml.py is non-existent

--- kcloud-setup/deploy.py
+++ kcloud-setup/deploy.py
@@ -4,7 +4,6 @@ from pyinfra.operations import apt, files, git, server
 local.include("system-setup.py")
 local.include("llvm.py")
 local.include("ocaml.py")
-local.include("checkml.py")
 local.include("dafny.py")
 local.include("souffle.py")
 local.include("bitnet.py")
